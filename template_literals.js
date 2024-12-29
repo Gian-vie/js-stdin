538a7a35c2d647e07800105fa06acdf6791ab088
Gian Vieceli <gianvieceli@gmail.com>
2024-12-29 16:34:14 -0300

    templates

---
+++ template_literals.js
@@ -0,0 +1,7 @@
+let firstName = "joe"
+
+const email = 'Hi ' + firstName + '\nThe meeting is confirmed \nAndre' // o "\n" é equivalente a um enter
+const email2 = `Hi ${firstName}
+The meeting is confirmed
+Andre`
+// a crase remove a necessidade de uso dos + pra adicionar variaveis e do \n para dar enter
