50098475214e0b365b1abb26d12bd29cd6485862
Gian Vieceli <gianvieceli@gmail.com>
2024-12-29 15:50:34 -0300

    factory pronta

--- factory_obj.js
+++ factory_obj.js
@@ -2,13 +2,13 @@
 // FACTORYs são como "fabricas" que geram esses objetos de froma automatica.
 
 /** EXEMPLO DE OBJETO
- * 
- * const book {
+ *
+ * const book = {
  *      bookTitle: "titulo do livro",
  *      bookAutor: "Nome do Autor",
  *      bookPages: "numero de paginas",
  *      printBook: function() { // pode ter funçoes dentro dos Objetos
- *          console.log("printing...") 
+ *          console.log("printing...")
  *      },
  *      bookCapters: { // pode ter objetor dentro de objetos
  *          capter1: "capitulo 1"
@@ -18,4 +18,19 @@
  * }
  */
 
+// Factory 
+function createBook(title, author, pages) {
+  const book = {
+    bookTitle: title,
+    bookAutor: author,
+    bookPages: pages,
+    printBook: function () {
+      console.log("printing...");
+    },
+  };
+  return book // declarando o retorno dessa função
+}
 
+
+const book1 = createBook("atomic", "james", 306)
+const book2 = createBook("think", "Napolean", 450)
