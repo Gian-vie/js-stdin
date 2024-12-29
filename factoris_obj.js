2052ee10ba07b63fa83465c6de4f91fe6501e44c
Gian Vieceli <gianvieceli@gmail.com>
2024-12-29 15:42:19 -0300

    primeiro commit js em  dias

---
+++ factoris_obj.js
@@ -0,0 +1,21 @@
+//Objetos contem varias informaçoes (variaveis) internamente de forma organizada.
+// FACTORYs são como "fabricas" que geram esses objetos de froma automatica.
+
+/** EXEMPLO DE OBJETO
+ * 
+ * const book {
+ *      bookTitle: "titulo do livro",
+ *      bookAutor: "Nome do Autor",
+ *      bookPages: "numero de paginas",
+ *      printBook: function() { // pode ter funçoes dentro dos Objetos
+ *          console.log("printing...") 
+ *      },
+ *      bookCapters: { // pode ter objetor dentro de objetos
+ *          capter1: "capitulo 1"
+ *          capter2: "capitulo 2"
+ *          capter3: "capitulo 3"
+ *      }
+ * }
+ */
+
+
