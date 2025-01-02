4c4ea237cb944fd752e90f9a31fa2ceb651d107b
Gian Vieceli <gianvieceli@gmail.com>
2025-01-01 23:39:17 -0300

    clean testes

--- functions.js
+++ functions.js
@@ -59,3 +59,61 @@ function paresEm(num){
     }
 };
 
+
+function isPalindrome(text){
+    let invertido = text.split('');
+    let original = text.split('');
+    invertido.reverse()
+    invertido = invertido.toString()
+    original = original.toString() 
+    if(original == invertido){
+        return true
+    }else {
+        return false
+    }
+};
+
+
+function calculateAverage(arr){
+    let soma = 0
+    if(arr.length === 0){
+        return 0
+    }
+    for(let i = 0; i < arr.length; i++){
+        soma += arr[i]
+    }
+    let average = total/arr.length
+    return average
+}
+
+
+function reverseString (text){
+    let invertido = text.split("");
+    let reverse = invertido.reverse()
+    let fim = reverse.join("")
+    return fim
+}
+
+function countVowels(text){
+    let vowels = 0
+    let toArray = text.split("");
+    for(let i = 0; i < toArray.length; i++){
+        if(toArray[i] == 'a' || toArray[i] == 'e' ||toArray[i] == 'i' ||toArray[i] == 'o' ||toArray[i] == 'u' ){
+            vowels++
+        }
+    }
+    return vowels
+}
+
+function sumEvenNumbers (arr){
+    let soma = 0
+    if(arr.length === 0){
+        return 0
+    }
+    for(let i = 0; i < arr.length; i++){
+       if(arr[i] % 2 === 0){
+        soma += arr[i]
+       }
+    }
+    return soma
+}
