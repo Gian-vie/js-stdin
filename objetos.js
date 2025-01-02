ea49ab06a2917237098a361530b84ea26583eb73
Gian Vieceli <gianvieceli@gmail.com>
2025-01-02 14:32:40 -0300

    obj basics

---
+++ objetos.js
@@ -0,0 +1,43 @@
+let carro = {
+    marca: "VW",
+    portas: 4,
+    eletrico: false,
+    motor: 1.0
+}
+
+// console.log(carro.portas); // acessa o valor de portas no objeto carro
+
+delete carro.portas; // deleta portas
+
+// console.log(carro.portas); //retorna undefined
+
+carro.tetoSolar = true; // add tetoSolar com valor true
+
+let adicionais = {
+    arcondicionado: true,
+}
+
+Object.assign(carro, adicionais) //copia os dados do segundo obj no primeiro
+
+let objA = {
+    chave1: 1,
+    cahve2: 2
+}
+
+Object.keys(objA)
+// retorna (chave1, chave2)
+
+//// mutação
+
+let pessoa = {
+    nome:"joao"
+}
+
+let pessoa2 = pessoa
+
+pessoa2.nome = "Pedro" 
+// altera de ambos, tanto pessoa quanto pessoa2
+
+pessoa.nome = "Matheus" 
+// altera de ambos, tanto pessoa quanto pessoa2
+
