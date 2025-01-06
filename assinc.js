cedd47671d5ec2552338168d8b0af7be84df365a
Gian Vieceli <gianvieceli@gmail.com>
2025-01-06 14:38:28 -0300

    assinc calback

---
+++ assinc.js
@@ -0,0 +1,9 @@
+// callback
+
+// console.log("1")
+
+// setTimeout(() => console.log("5") ,2000)
+// // vai ser executado 2 segundos depois
+// console.log("2")
+// console.log("3")
+// console.log("4")
