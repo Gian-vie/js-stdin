431162272321d2a30607ac53d8452a537524d2b5
Gian Vieceli <gianvieceli@gmail.com>
2025-01-06 23:39:08 -0300

    game develment

--- assinc.js
+++ assinc.js
@@ -2,8 +2,54 @@
 
 // console.log("1")
 
-// setTimeout(() => console.log("5") ,2000)
+// setTimeout(() => {console.log("5")} ,2000)
 // // vai ser executado 2 segundos depois
 // console.log("2")
 // console.log("3")
 // console.log("4")
+
+//promises
+
+// let promessa = Promise.resolve(5);
+
+// console.log(promessa) // retorna resolvida como um obj
+// promessa.then((value) => {console.log(`O valor é ${value}`)})
+// .then((value) => {value + 5});
+
+//then catch
+
+// let p = Promise.resolve(new Error("Não deu certo"))
+
+// console.log("asdf")
+
+// p.then(value => console.log(value)) //falha
+// .catch(reason => console.log("falhou "+ reason))
+
+
+//reject promise
+
+// function verificarNumero(num){
+//     return new Promise((resolve, reject) => {
+//         if(num === 2){
+//             resolve(console.log(`o numero é ${num}`))
+//         } else {
+//             reject(new Error("falhou"))
+//         }
+//     })
+// }
+
+
+// .all  faz uma array de variaveis assincronas e respeira a ordem e o tempo para resolver
+
+
+//async function
+
+// async function somar(a,b){
+//     return a + b
+// }
+
+// soamr(2,4).then(value => console.log(value))
+
+
+// await 
+//espera a promise ser resolvida
---
+++ jogo/js/scripts.js
@@ -0,0 +1,27 @@
+let x = document.querySelector(".x");
+let o = document.querySelector(".o");
+let boxes = document.querySelector(".box");
+let button = document.querySelector("#button-container button");
+let messageContainer = document.querySelector("#message");
+let messageText = document.querySelector("#message p");
+let secondPlayer;
+
+//contador de jogadas
+let player1 = 0;
+let player2 = 0;
+
+//adicionar o evento de click aos boxes
+for (let i = 0; i < boxes.length; i++) {
+  //quando clica na caixa
+  boxes[i].addEventListener("click", function() {
+    let el;
+    if (player1 == player2) {
+      //x
+      el = x;
+    } else {
+      el = o;
+    }
+    let cloneEl = el.cloneNode(true);
+    this.appendChild(cloneEl);
+  });
+}
